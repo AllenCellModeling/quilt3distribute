c73ff9a63034499c32a84eb1a81ae5c880b6cb52
Jackson Brown <jmaxfieldbrown@gmail.com>
2019-06-07 14:58:08 -0700

    Remove comment leftover from cookiecutter

--- t4distribute/bin/t4_distribute_dataset.py
+++ t4distribute/bin/t4_distribute_dataset.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-This sample script will get deployed in the bin directory of the
-users' virtualenv when the parent module is installed using pip.
-"""
-
 import sys
 import argparse
 import logging
