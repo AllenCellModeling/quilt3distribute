b1c01e3c5a8c68f975dd8d0dc4d8797813d1398f
Jackson Brown <jmaxfieldbrown@gmail.com>
2019-07-01 12:06:12 -0700

    Fix create unique logical key test to allow any machine's hash

--- quilt3distribute/tests/test_file_utils.py
+++ quilt3distribute/tests/test_file_utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
+
 import pytest
 
 from quilt3distribute import file_utils
@@ -12,5 +14,18 @@ def example_csv(data_dir):
 
 
 def test_create_unique_logical_key(example_csv):
-    assert file_utils.create_unique_logical_key(example_csv) == "9023f477_example.csv"
-    assert file_utils.create_unique_logical_key(str(example_csv)) == "9023f477_example.csv"
+    # Create examples of what should be the same logical key but one with pathlib.Path and the other a string
+    path_created_logical_key = file_utils.create_unique_logical_key(example_csv)
+    str_created_logical_key = file_utils.create_unique_logical_key(str(example_csv))
+
+    # Should be 8 characters of hex followed by the filename
+    regexp = re.compile("[a-z0-9]{8}_example.csv")
+
+    # Assert that both generated logical keys match the pattern
+    # We can't just use a predetermined hash here because depending on the testing machine, the fully resolved
+    # filepath may be different
+    assert regexp.match(path_created_logical_key)
+    assert regexp.match(str_created_logical_key)
+
+    # Assert that the two logical keys are the same
+    assert path_created_logical_key == str_created_logical_key
