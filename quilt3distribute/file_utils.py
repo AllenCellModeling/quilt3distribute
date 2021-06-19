b25bab9ce7f97bc14577a26375258c285c4145ec
Jianxu Chen <jxchen.ustc@gmail.com>
2021-06-19 14:40:37 -0700

    taking full sha256 key for unique file name

--- quilt3distribute/file_utils.py
+++ quilt3distribute/file_utils.py
@@ -11,7 +11,7 @@ def create_unique_logical_key(physical_key: Union[str, Path]) -> str:
     pk = Path(physical_key).expanduser().resolve(strict=True)
 
     # Creat short hash from fully resolved physical key
-    short_hash = hashlib.sha256(str(pk).encode("utf-8")).hexdigest()[:8]
+    short_hash = hashlib.sha256(str(pk).encode("utf-8")).hexdigest()
 
     # Return the unique logical key
     return f"{short_hash}_{pk.name}"
